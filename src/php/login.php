ff644d392908043dfbf581d8b95408cea50bdeb6
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-04 18:41:41 -0300

    Feat: Criando autenticação com dados MySQL
    * - Autenticação feita utilizando dados MySQL. Condicionais caso os dados foram aceitos ou negados - caso forem negados é enviado via URL um código de error=1 ao arquivo index.php

--- src/php/login.php
+++ src/php/login.php
@@ -1,16 +1,37 @@
 <?php 
-include '../conf/MySQL.php';
+include '../conf/MySQL.php'; //Incluindo conexão com Bando de Dados MYSQL (MySQL.php)
 
-echo "Redirecionamento Concluido com sucesso :)";
-
+//Variaveis do formulário - index.php
 $user = $_POST['input-user'];
 $senha = $_POST['input-password'];
 
-if ($user == "lucas.feitosa" && $senha == "Si!car@2024") {
-	header("location: ../../index.php");
-	die("Erro inesperado aconteceu");
-} else {
-	echo "<br>O usuário é: ".$user;
-	echo "<br>A senha é: ".$senha;
+$sql = "SELECT * FROM usuarios"; //Variavel comando MySQL
+$result = $link->query($sql); //Variavel para atribuir o comando MySQL para o link de conexão (MySQL.php)
+
+//Caso o numeros de colunas da variavel for MAIOR que ZERO 
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()){ 
+		if ($user == $row['nome_usuario'] && $senha == $row['senha']) {
+			header("location: ../../index.php");
+			die("Erro inesperado aconteceu".error);
+		} else {
+			header("location: ../../index.php?error=1");
+			echo "Usuario ou senha incorreto";
+			/*echo "<br>O usuário digital foi: ".$user;
+			echo "<br>A senha digitada foi: ".$senha;
+
+			echo "<br>";
+
+			echo "<br>ID -> ". $row['id']. "<br>";
+			echo "<br>USER -> ". $row['nome_usuario']. "<br>";
+			echo "<br>PASSWORD -> ". $row['senha']. "<br>";*/
+		}
+
+		
+	}
+}else{ //Caso o numeros de colunas da variavel for MENOR que ZERO 
+	echo "<br>Sem resultados :(<br>";
 }
+
+echo "<br>Redirecionamento Concluido com sucesso :)";
 ?>
