1b06f99fa4bdd0bb64d1a829b94a42d52dc5d928
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-04 17:06:50 -0300

    Fix: Adicionando arquivos login.php, index.css
    Login.php - Adicionando arquivo teste para redirecionamento e autenticação de usuario e senha
    
    index.css - Testando a linkagem do arquivo apenas deixando o background-color black e a cor da fonte branca

---
+++ src/php/login.php
@@ -0,0 +1,14 @@
+<?php 
+echo "Redirecionamento Concluido com sucesso :)";
+
+$user = $_POST['input-user'];
+$senha = $_POST['input-password'];
+
+if ($user == "lucas.feitosa" && $senha == "Si!car@2024") {
+	header("location: ../../index.php");
+	die("Erro inesperado aconteceu");
+} else {
+	echo "<br>O usuário é: ".$user;
+	echo "<br>A senha é: ".$senha;
+}
+?>
