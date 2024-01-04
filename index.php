54c6a093a873d422582f48bcc1f1228a479222a7
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-04 16:14:10 -0300

    feat: Commit Inicial
    * - Criando divs, labels e inputs para o sistema de login inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="src/styles/index.css">
+	<title>Atomic - Login</title>
+</head>
+<body>
+	<main class="main-class-master">
+		<div class="box-class-container">
+			<div class="box-class">
+				<div class="box-login">
+					<div class="div-label-user">
+						<label>Usúario</label>
+					</div>
+					<div class="div-label-input-user">
+						<input type="text" placeholder="Informe seu usuário">
+					</div>
+					<div class="div-label-password">
+						<label>Senha</label>
+					</div>
+					<div class="div-label-input-password">
+						<input type="password" placeholder="Informe sua senha">
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</body>
+</html>
