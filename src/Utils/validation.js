ee6ed05cb032270762832c892665fd289bbb4fd0
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-09 18:41:50 -0300

    feat: validantion.js
    * - Adicionando um arquivo de validação javascript

---
+++ src/Utils/validation.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("#btn_cadastro");
+
+button.addEventListener("click", (e)=>{
+	let username = document.querySelector('#username_cadastro').value;
+	let password = document.querySelector('#password_cadastro').value;
+	let password_confirm = document.querySelector('#password_again_cadastro').value;
+
+	if (username.length < 10) {
+		alert("Seu nome de usuário deve ter mais de 10 caracteres!")
+		e.preventDefault();
+	}else if(password.length && password_confirm.length < 8){
+		alert('As senhas devem ter no mínimo 8 caracteres');
+		e.preventDefault();
+	}else if (password != password_confirm){
+		alert("As senhas não correspondem!");
+		e.preventDefault();
+	}
+
+	//console.log(`Username = ${username} - cont => ${username.length}\nPassword = ${password}\nConfirm Password = ${password_confirm}`);
+
+});
