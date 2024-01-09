9be23654a10c9958ebb1557b0170da87b7eca8e3
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-09 18:42:48 -0300

    fix: atualizando rota
    * - Atualizando rota de cadastro.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ function error_login(){ //Função para retornar div abaixo caso a URL contenha 
 					</form>
 					<div class="div-novo-usuario">
 						<hr>
-						<p>Não tem uma conta? - <a href="src/page/signup.php">Cadastrar novo usuário</a></p>
+						<p>Não tem uma conta? - <a href="src/page/cadastro.php">Cadastrar novo usuário</a></p>
 					</div>
 					
 				</div>
