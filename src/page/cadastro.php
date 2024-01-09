91bafa2967e74a7777fc208cfbc2a8f3f3dcb3fd
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-09 17:24:40 -0300

    feat: Removendo trechos
    * - Removendo trechos especificos que seria correspondente a página index.php (Login)

--- src/page/cadastro.php
+++ src/page/cadastro.php
@@ -16,34 +16,25 @@
 		<div class="box-class-container">
 			<div class="box-class">
 				<div class="box-login">
-					<form action="src/php/login.php" method="POST">
+					<form action="" method="POST">
 						<div class="div-label-user">
 							<label>Usuário</label>
 						</div>
 						<div class="input-group flex-nowrap m-2">
 						  <span class="input-group-text" id="addon-wrapping"></span>
-						  <input type="text" class="form-control" name="input-user" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" required>
+						  <input type="text" class="form-control" name="" placeholder="" aria-label="Username" aria-describedby="addon-wrapping" required>
 						</div>
 						<div class="div-label-password">
 							<label>Senha</label>
 						</div>
 						<div class="input-group flex-nowrap m-2">
 						  <span class="input-group-text" id="addon-wrapping"></span>
-						  <input type="password" class="form-control" name="input-password" placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping" required>
-						</div>
-						<?php error_login() ?> <!--Chamada da função PHP -->
-						<div class="div-forgout-password">
-							<p><a href="#" class="pe-auto">Esqueci a senha</a></p>
+						  <input type="password" class="form-control" name="" placeholder="" aria-label="Username" aria-describedby="addon-wrapping" required>
 						</div>
 						<div class="div-input-button">
 							<button type="submit" class="btn btn-warning">Acessar</button>
 						</div>
 					</form>
-					<div class="div-novo-usuario">
-						<hr>
-						<p>Não tem uma conta? - <a href="src/page/signup.php">Cadastrar novo usuário</a></p>
-					</div>
-					
 				</div>
 			</div>
 		</div>
