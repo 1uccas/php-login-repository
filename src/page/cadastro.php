a3849ee137df06e722130769b654c7664627d0cb
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-09 18:43:53 -0300

    feat: adicionando id
    * - Implementando id no botão de cadastro

--- src/page/cadastro.php
+++ src/page/cadastro.php
@@ -39,7 +39,7 @@
 						  <input type="password" class="form-control" id="password_again_cadastro" name="password_again_cadastro" placeholder="Confirm your password" aria-label="Username" aria-describedby="addon-wrapping" required>
 						</div>
 						<div class="div-input-button">
-							<button type="submit" class="btn btn-warning" name="btn_cadastro">Criar cadastro</button>
+							<button type="submit" id="btn_cadastro" class="btn btn-warning" name="btn_cadastro">Criar cadastro</button>
 						</div>
 					</form>
 				</div>
@@ -47,4 +47,5 @@
 		</div>
 	</main>
 </body>
+<script type="text/javascript" src="../Utils/validation.js"></script>
 </html>
