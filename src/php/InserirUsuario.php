a23563a2f7182af2fcda0b2cbe4d029606f99f72
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-10 17:16:59 -0300

    feat: Validação de conteudo no formulário php
    * - Validando inputs do formulário para evitar envio de inputs sem informação

--- src/php/InserirUsuario.php
+++ src/php/InserirUsuario.php
@@ -4,7 +4,31 @@ include '../conf/MySQL.php';
 //FAZER UMA VERIFICAÇÃO COM O BANCO DE DADOS PARA SABER SE O LOGIN E A SENHA DIGITADO JÁ EXISTE!
 //ENVIAR UM CODIGO DE ERRO PELA URL PARA ENVIO DE ECHO A PÁGINA DE CADASTRO, INFORMADO DO OCORRIDO!
 //CRIPTOGRAFAR URL INSERIRUSUARIO.PHP
+//FAZER ALGO PARA QUE SIMPLESMENTE NÃO POSSA DIGITAR A URL INSERIRUSUARIO.PHP E CADASTRAR USER SEM PASSAR PELO FORMULÁRIO (OK)
 
+//Valida se o formulário contem alguma informação
+if (!isset($_POST['username_cadastro']) || !isset($_POST['password_cadastro'])) {
+   die("Usuario ou senha não encontrado ou digitos < 1");
+}else{
+   $user = mysqli_real_escape_string($link, $_POST['username_cadastro']); // Sanitizar entrada do usuário
+   $password = mysqli_real_escape_string($link, $_POST['password_cadastro']); // Sanitizar entrada do usuário
+}
+
+//chamada de usuarios do banco de dados
+$user_sql = "SELECT * FROM usuarios";
+$result_sql = $link->query($user_sql);
+
+// Caso o número de colunas da variável for MAIOR que ZERO 
+if ($result_sql->num_rows > 0) {
+    while ($row = $result_sql->fetch_assoc()) {
+        print_r($row);
+    }
+} else { 
+    // Caso o número de colunas da variável for MENOR que ZERO 
+    echo "<br>Sem resultados :(<br>";
+}
+
+/*
 $id = uniqid();
 $user = mysqli_real_escape_string($link, $_POST['username_cadastro']); // Sanitizar entrada do usuário
 $password = mysqli_real_escape_string($link, $_POST['password_cadastro']); // Sanitizar entrada do usuário
@@ -23,4 +47,5 @@ if (mysqli_query($link, $sql)) {
 }
 
 mysqli_close($link);
+*/
 ?>
