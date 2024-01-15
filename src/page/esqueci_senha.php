42a8f2cc4a5781f07c3502d24124bbabd70c6bf9
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-15 17:24:19 -0300

    feat: formulario de envio para email

---
+++ src/page/esqueci_senha.php
@@ -0,0 +1,5 @@
+<form action="enviar_email" method="post">
+      <input type="text" name="user" placeholder="Nome de Usuario">
+      <input type="email" name="email" placeholder="E-mail">
+      <input type="submit" name="Enviar">
+</form>
