d3f4df49c254ca94858e11be1be2a8fefd406db0
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-16 18:18:29 -0300

    feat: incluindo arquivo validarToken

---
+++ src/page/acesso_unico.php
@@ -0,0 +1,7 @@
+<?php 
+	include 'src/php/validarToken.php';
+?>
+<form action="validarToken" method="post">
+      <input type="text" name="cd_uniq" placeholder="Informe seu codigo de uso unico">
+      <input type="submit" name="Enviar">
+</form>
