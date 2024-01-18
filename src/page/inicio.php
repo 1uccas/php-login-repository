8a3c2f20a0fd2d6801d3c920a26ed1e543854605
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-18 16:34:15 -0300

    fix: condicional a inicialização de Sessions

--- src/page/inicio.php
+++ src/page/inicio.php
@@ -1,5 +1,11 @@
 <?php 
-session_start();
+if (!isset($_SESSION)) {
+	header("location: /?error=3"); //CODIGO VIA URL INFORMANDO AO USUARIO PARA FAZER LOGIN
+	die("Sessão não ativa!"); 
+}else{
+	session_start();
+
+}
 echo "Olá "."<strong>".$_SESSION['nome']."</strong>";
 echo '<br>Este é o ID da sua sessão:'."<strong>".$_SESSION['id']."</strong>";
 
