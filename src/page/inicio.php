3d4824653f5015d6c28f8d4a153bb01ea9cb0489
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-18 17:29:04 -0300

    fix: iniciando o session antes da condional

--- src/page/inicio.php
+++ src/page/inicio.php
@@ -1,15 +1,17 @@
 <?php 
-if (!isset($_SESSION)) {
+session_start();
+
+if (!isset($_SESSION['id'])) {
 	header("location: /?error=3"); //CODIGO VIA URL INFORMANDO AO USUARIO PARA FAZER LOGIN
 	die("Sessão não ativa!"); 
 }else{
-	session_start();
+	echo "Olá "."<strong>".$_SESSION['nome']."</strong>";
+	echo '<br>Este é o ID da sua sessão:'."<strong>".$_SESSION['id']."</strong>";
+
+	echo "<br>Hello World!";
+
+	echo "<br><br> <h1>Implementar sessões na página inicio!!!!!</h1>";
 
 }
-echo "Olá "."<strong>".$_SESSION['nome']."</strong>";
-echo '<br>Este é o ID da sua sessão:'."<strong>".$_SESSION['id']."</strong>";
 
-echo "<br>Hello World!";
-
-echo "<br><br> <h1>Implementar sessões na página inicio!!!!!</h1>";
 ?>
