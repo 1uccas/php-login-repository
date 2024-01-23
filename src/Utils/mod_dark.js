1fcad6df9e6e1c2a49f05313527f3dc4fd0507ec
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-23 18:59:42 -0300

    feat: implementando modo dark em inicio.php

--- src/Utils/mod_dark.js
+++ src/Utils/mod_dark.js
@@ -6,10 +6,12 @@ const body = document.querySelector(".bg-dark");
 const buttonWhite = document.querySelector("#button_white");
 const buttonDark = document.querySelector("#button_dark");
 const boxLogin = document.querySelector('.box-login');
+let test = document.querySelector('.div-initil-left');
 
 // função para aplicar o modo
 const applyMode = (mode) => {
   body.className = mode === 'dark' ? "bg-dark" : "bg-white";
+  test.className = mode === 'dark' ? "div-initil_white_mod" : "div-initil";
   buttonDark.style.display = mode === 'dark' ? "none" : "block";
   buttonWhite.style.display = mode === 'dark' ? "block" : "none";
 };
--- src/page/inicio.php
+++ src/page/inicio.php
@@ -1,17 +1,26 @@
 <?php 
+include 'src/Utils/mods.html';
 session_start();
 
 if (!isset($_SESSION['id'])) {
 	header("location: /?error=3"); //CODIGO VIA URL INFORMANDO AO USUARIO PARA FAZER LOGIN
 	die("Sessão não ativa!"); 
 }else{
-	echo "Olá "."<strong>".$_SESSION['nome']."</strong>";
-	echo '<br>Este é o ID da sua sessão: '."<strong>".$_SESSION['id']."</strong>";
-
-	echo "<br><br>Hello World!<br>";
-
-	echo "<br><a href='exit'>Sair</a>";
+	$nomeSession = $_SESSION['nome'];
+	$idSession = $_SESSION['id'];
 }
 
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="../../src/styles/inicio.css">
+<body class="bg-dark">
+	<div class="div-initil">
+		<label>Olá, <strong><?php echo $nomeSession; ?></strong></label><br>
+		<label>ID - <strong><?php echo $idSession; ?></strong></label>	
+	</div>
+</body>
+
+
+<p><a href='exit'>Sair</a></p>
+<script type="text/javascript" src="../../src/Utils/mod_dark.js"></script>
 
