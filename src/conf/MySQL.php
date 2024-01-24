af6841b96d70f56a6c88b00675641a5c1e2a45c8
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-24 18:11:16 -0300

    fix: transformando MySQL em POO

--- src/conf/MySQL.php
+++ src/conf/MySQL.php
@@ -1,5 +1,38 @@
 <?php 
-header("Content-Type: text/html; charset=utf-8");
+class DatabaseConnection {
+    private $link;
+    
+    const SERVIDOR = "localhost";
+    const USUARIO = "root";
+    const SENHA = "";
+    const BANCO = "atomic";
+
+    public function __construct() {
+        $this->link = mysqli_connect(self::SERVIDOR, self::USUARIO, self::SENHA, self::BANCO);
+
+        // Valida a conexão
+        if (mysqli_connect_errno()){
+            echo "Falha ao conectar no Banco de Dados MySQL: " . mysqli_connect_error();
+        }
+
+        // Configuração do conjunto de caracteres
+        mysqli_query($this->link, "SET NAMES 'utf8'") or die("Erro na SQL" . mysqli_error($this->link));
+        mysqli_query($this->link, "SET character_set_connection=utf8") or die("Erro na SQL" . mysqli_error($this->link));
+        mysqli_query($this->link, "SET character_set_client=utf8") or die("Erro na SQL" . mysqli_error($this->link));
+        mysqli_query($this->link, "SET character_set_results=utf8") or die("Erro na SQL" . mysqli_error($this->link));
+    }
+
+    public function getLink() {
+        return $this->link;
+    }
+
+    public function closeConnection() {
+        mysqli_close($this->link);
+    }
+}
+
+
+/*header("Content-Type: text/html; charset=utf-8");
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
@@ -12,6 +45,6 @@ if (mysqli_connect_errno()){
 mysqli_query($link, "SET NAMES 'utf8'") or die("Erro na SQL" . mysqli_error($link));
 mysqli_query($link, "SET character_set_connection=utf8") or die("Erro na SQL" . mysqli_error($link));
 mysqli_query($link, "SET character_set_client=utf8") or die("Erro na SQL" . mysqli_error($link));
-mysqli_query($link, "SET character_set_results=utf8") or die("Erro na SQL" . mysqli_error($link));
+mysqli_query($link, "SET character_set_results=utf8") or die("Erro na SQL" . mysqli_error($link));*/
 
 ?>
