d6d51e49677e6a08f78b95c0c90d086184049ab2
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-28 18:30:34 -0300

    fix: removendo comentários

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 
 $url = (!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT)) ? filter_input(INPUT_GET, 'url', FILTER_DEFAULT) : "login");
-var_dump($url);
+//var_dump($url);
 
-//
 
 $url = array_filter(explode('/', $url));
-print_r($url);
+//print_r($url);
 
 $arquivo = 'src/page/'.$url[0].'.php';
 $arquivo_php = 'src/php/'.$url[0].'.php';
-var_dump("<br>".$arquivo);
+//var_dump("<br>".$arquivo);
 
 if (is_file($arquivo)) {
 	include $arquivo;
