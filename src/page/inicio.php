9a72e9d4ac56b2ba03e7a98ed85b113e1a4808ea
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-28 20:25:18 -0300

    style: estilizando inicio.php

--- src/page/inicio.php
+++ src/page/inicio.php
@@ -16,13 +16,17 @@ $idCrypt = base64_encode($idSession);
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link rel="stylesheet" href="../../src/styles/inicio.css">
 <body class="bg-dark">
-	<div class="div-initil">
-		<label>Olá, <strong><?php echo $nomeSession; ?></strong></label><br>
-		<label>ID - <strong><?php echo $idSession; ?></strong></label>	
-	</div>
+	<main class="master">
+		<div class="div-initil">
+			<label>Olá, <strong><?php echo $nomeSession; ?></strong> 🐱‍🐉</label><br>
+			<!--<label>ID - <strong><?php echo $idSession; ?></strong></label>-->
+			<p><a href='exit/di=<?php echo $idCrypt; ?>'>Sair</a></p>
+		</div>
+	</main>
+	
 </body>
 
 
-<p><a href='exit/di=<?php echo $idCrypt; ?>'>Sair</a></p>
+
 <script type="text/javascript" src="../../src/Utils/mod_dark.js"></script>
 
