9247415c7e08e5c00c8a6f2d190e2fa843dba024
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-29 17:45:25 -0300

    feat(acesso_unico): incluindo links (html)

--- src/page/acesso_unico.php
+++ src/page/acesso_unico.php
@@ -3,6 +3,8 @@ $email = $_GET['liame'];
 $descrypt_email = base64_decode($email);
 
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="../../src/styles/acesso_unico.css">
 <form action="../validarToken" method="post">
       <p>Enviamos um código de acesso único ao e-mail: <strong><?php echo $descrypt_email; ?></strong></p>
       <input type="text" name="cd_uniq" placeholder="Informe seu codigo de uso unico">
