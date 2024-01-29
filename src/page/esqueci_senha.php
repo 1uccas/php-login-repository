24155b39b2c0132554b35e734ba9dd1173f03671
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-29 17:07:30 -0300

    style(esqueci_senha): definindo conteudos como bootstrap

--- src/page/esqueci_senha.php
+++ src/page/esqueci_senha.php
@@ -1,4 +1,7 @@
-<form action="email" method="post">
-      <input type="text" name="user" placeholder="Nome de Usuario">
-      <input type="submit" name="Enviar">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<form action="email" method="post" class="form">
+      <div class="input-group flex-nowrap w-25">
+        <input type="text" class="form-control" id="username_cadastro" name="user" placeholder="Nome de Usuario" aria-label="Username" aria-describedby="addon-wrapping" required>
+      </div>
+      <button type="submit" id="btn_cadastro" class="btn btn-success" name="Enviar">Enviar</button>
 </form>
