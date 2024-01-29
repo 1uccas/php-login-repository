29f9ee07a31af92bdf3c2ade07645adc0b0fc12d
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-29 17:21:51 -0300

    feat(esqueci_senha): incluindo mods (js, html)

--- src/page/esqueci_senha.php
+++ src/page/esqueci_senha.php
@@ -1,3 +1,7 @@
+<?php 
+include 'src/Utils/mods.html';
+
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link rel="stylesheet" href="../../src/styles/esqueci_senha.css">
 
@@ -9,3 +13,4 @@
             <button type="submit" id="btn_cadastro" class="btn btn-success w-25" name="Enviar">Enviar</button>
       </form>
 </body>
+<script type="text/javascript" src="../../src/Utils/mod_dark.js"></script>
