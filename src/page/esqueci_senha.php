d7dcd93e4a02de8619e20f54b947f3a36211792e
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-29 17:10:57 -0300

    style(esqueci_senha): adicionando body class dark

--- src/page/esqueci_senha.php
+++ src/page/esqueci_senha.php
@@ -1,7 +1,11 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<form action="email" method="post" class="form">
-      <div class="input-group flex-nowrap w-25">
-        <input type="text" class="form-control" id="username_cadastro" name="user" placeholder="Nome de Usuario" aria-label="Username" aria-describedby="addon-wrapping" required>
-      </div>
-      <button type="submit" id="btn_cadastro" class="btn btn-success" name="Enviar">Enviar</button>
-</form>
+<link rel="stylesheet" href="../../src/styles/esqueci_senha.css">
+
+<body class="bg-dark">
+      <form action="email" method="post" class="form">
+            <div class="input-group flex-nowrap w-25">
+              <input type="text" class="form-control" id="username_cadastro" name="user" placeholder="Nome de Usuario" aria-label="Username" aria-describedby="addon-wrapping" required>
+            </div>
+            <button type="submit" id="btn_cadastro" class="btn btn-success" name="Enviar">Enviar</button>
+      </form>
+</body>
